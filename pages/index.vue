bd7590fdbebd1fd342e468f3c23b365ae5f35449
Lucas Lima <lucascglima@gmail.com>
2022-04-19 15:36:22 -0300

    Add mirage and faker api

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,11 @@
       <div
         class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6"
       >
-        <product-card />
+        <product-card
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+        />
       </div>
     </div>
   </main>
@@ -16,5 +20,14 @@
 import ProductCard from '@/components/ProductCard'
 export default {
   components: { ProductCard },
+  data() {
+    return {
+      products: [],
+    }
+  },
+  async created() {
+    this.products = (await this.$axios.get('/api/products')).data.products
+    await this.$axios.get('/api/users')
+  },
 }
 </script>
