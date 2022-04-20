ed88f1772386a32009c4cbf19f58b0cf83898ae4
Lucas Lima <lucascglima@gmail.com>
2022-04-20 12:00:18 -0300

    main

--- miragejs/factories/product.js
+++ miragejs/factories/product.js
@@ -28,7 +28,6 @@ const images = [
   'https://images.unsplash.com/photo-1495857000853-fe46c8aefc30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
   'https://images.unsplash.com/photo-1444881421460-d838c3b98f95?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80',
 ];
-
 export default {
   product: Factory.extend({
     title() {
